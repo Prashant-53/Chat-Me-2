a45ac358e63101f6b91b958d3701ee5ea10c9953
Prashant-53 <prashantsahu120503@gmail.com>
2026-04-10 18:09:45 +0530

    login and signup fetch fix

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -13,7 +13,7 @@ function Login(){
   const handleLogin = async() =>{
 
     try{
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/auth/login`, {
         method: "POST",
         headers:{
             "Content-Type": "application/json",
