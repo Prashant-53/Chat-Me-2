5b77d9192d12bee09164b2884cc0770dbaca20d5
Prashant-53 <prashantsahu120503@gmail.com>
2026-04-10 23:45:34 +0530

    mongodb fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 
     try {
-        await mongoose.connect("mongodb+srv://prashantsahu120503_db_user:<1T11EdvtuR0BqJEa>@cluster0.7tzgmt6.mongodb.net/?appName=Cluster0");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("mongoDB connected");
     }
     catch (error) {
